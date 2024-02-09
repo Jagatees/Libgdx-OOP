77238487c015f05d09fbd0496bd47e1b2a34d6d1
K Jagateesvaran Rajoo <140966272+Jagatees@users.noreply.github.com>
2024-02-09 23:46:13 +0800

    Add : Playercontroller

---
+++ OOPA1/core/src/com/mygdx/game/Entity/PlayerController.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.Entity;
+
+public class PlayerController{
+    private Player player;
+
+    public PlayerController(Player player) {
+        this.player = player;
+    }
+
+    Player getPlayer(){
+        return this.player;
+    }
+
+    public void Moveup(){
+
+    }
+
+
+}
