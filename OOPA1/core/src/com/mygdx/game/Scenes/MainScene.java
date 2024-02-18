19953a70c626ea965a0f593a4508034392e83dfe
K Jagateesvaran Rajoo <140966272+Jagatees@users.noreply.github.com>
2024-02-19 01:18:02 +0800

    Add : Game Controller

--- OOPA1/core/src/com/mygdx/game/Scenes/MainScene.java
+++ OOPA1/core/src/com/mygdx/game/Scenes/MainScene.java
@@ -1,15 +1,10 @@
 package com.mygdx.game.Scenes;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Canvas.CanvasManager;
 import com.mygdx.game.Canvas.MainMeunCanvas;
-import com.mygdx.game.Entity.Entity;
-import com.mygdx.game.Entity.EntityManager;
-import com.mygdx.game.Entity.nonPlayer;
+
 
 public class MainScene implements Scene{
 
