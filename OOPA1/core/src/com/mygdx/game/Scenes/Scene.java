c754c55ee9e7134138ad7092720af1ba482faa34
K Jagateesvaran Rajoo <140966272+Jagatees@users.noreply.github.com>
2024-02-18 23:44:25 +0800

    Comment : Scene

--- OOPA1/core/src/com/mygdx/game/Scenes/Scene.java
+++ OOPA1/core/src/com/mygdx/game/Scenes/Scene.java
@@ -1,8 +1,21 @@
 package com.mygdx.game.Scenes;
 
+
+
+/**
+ * @Description :
+ */
 public interface Scene {
+
+    /**  */
     void create();
+
+    /**  */
     void render();
+
+    /**  */
     void update(float delta);
+
+    /**  */
     void dispose();
 }
