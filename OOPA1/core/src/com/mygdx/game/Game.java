5c34152a94ee30a730bb3a38092f05c0a479d7a2
K Jagateesvaran Rajoo <140966272+Jagatees@users.noreply.github.com>
2024-02-18 23:29:30 +0800

    Comment : Template Game.java

--- OOPA1/core/src/com/mygdx/game/Game.java
+++ OOPA1/core/src/com/mygdx/game/Game.java
@@ -1,39 +1,57 @@
 package com.mygdx.game;
 
+// Import statements for libGDX framework and game scene management
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.mygdx.game.Input.Keyboard;
-import com.mygdx.game.Scenes.GameScene;
 import com.mygdx.game.Scenes.MainScene;
 import com.mygdx.game.Scenes.SceneManager;
 
 
-
+/**
+ * The Game class serves as the core of the application, managing the lifecycle
+ * and transitions between different scenes within the game. It extends
+ * ApplicationAdapter, utilizing libGDX's lifecycle methods to manage game states.
+ */
 public class Game extends ApplicationAdapter {
 
-	private SceneManager sceneManager;
-	private Keyboard keyboard;
+	/** SceneManager instance to manage transitions and states of various scenes */
+	private final SceneManager sceneManager = new SceneManager();
 
+	/**
+	 * @Description :
+	 * @param :
+	 * @return :
+	 */
 	@Override
 	public void create() {
-		sceneManager = new SceneManager();
-		keyboard = new Keyboard();
 		sceneManager.setScene(new MainScene(sceneManager));
 	}
 
+	/**
+	 * @Description :
+	 * @param :
+	 * @return :
+	 */
 	@Override
 	public void render() {
-		if (keyboard.isKeyPressedBool(Input.Keys.NUM_1)) {
-			sceneManager.setScene(new MainScene(sceneManager));
-		} else if (keyboard.isKeyPressedBool(Input.Keys.NUM_2)) {
-			sceneManager.setScene(new GameScene(sceneManager));
-		}
-
 		sceneManager.render();
-		sceneManager.update(Gdx.graphics.getDeltaTime());
+		update(Gdx.graphics.getDeltaTime());
 	}
 
+	/**
+	 * @Description :
+	 * @param :
+	 * @return :
+	 */
+	private void update(float deltaTime) {
+		sceneManager.update(deltaTime);
+	}
+
+	/**
+	 * @Description :
+	 * @param :
+	 * @return :
+	 */
 	@Override
 	public void dispose() {
 		sceneManager.dispose();
--- OOPA1/core/src/com/mygdx/game/Input/Keyboard.java
+++ OOPA1/core/src/com/mygdx/game/Input/Keyboard.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 public class Keyboard {
 
     public Keyboard() {
-        System.out.println("Init Keyboard");
+        System.out.println("Keyboard Class Init");
     }
 
     public boolean isKeyPressedBool(int keycode) {
