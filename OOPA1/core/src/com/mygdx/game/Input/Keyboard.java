697f0bae8541f1bcb0410e49790cc66c38080aec
K Jagateesvaran Rajoo <140966272+Jagatees@users.noreply.github.com>
2024-02-19 11:58:20 +0800

    Comment Keyboard

--- OOPA1/core/src/com/mygdx/game/Input/Keyboard.java
+++ OOPA1/core/src/com/mygdx/game/Input/Keyboard.java
@@ -2,27 +2,41 @@ package com.mygdx.game.Input;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 
+
+/**
+ * Handles keyboard input detection for the game. Provides methods to check
+ * if specific keys are pressed and to identify any key press events.
+ */
 public class Keyboard {
 
+    /**
+     * Initializes a new instance of the Keyboard class.
+     * Prints a message to the console upon initialization for debugging purposes.
+     */
     public Keyboard() {
         System.out.println("Keyboard Class Init");
     }
 
+    /**
+     * Checks if a specific key is currently pressed.
+     *
+     * @param keycode The keycode of the key to check. Use constants from {@link Input.Keys}.
+     * @return true if the specified key is pressed, false otherwise.
+     */
     public boolean isKeyPressedBool(int keycode) {
         return Gdx.input.isKeyPressed(keycode);
     }
 
+
+    /**
+     * Checks if a specific key is currently pressed, returning an integer result.
+     *
+     * @param keycode The keycode of the key to check. Use constants from {@link Input.Keys}.
+     * @return 1 if the specified key is pressed, 0 otherwise.
+     */
     public int isKeyPressedInt(int keycode) {
         return Gdx.input.isKeyPressed(keycode) ? 1 : 0;
     }
 
-    public int getKeyPressed() {
-        for (int keycode = Input.Keys.A; keycode <= Input.Keys.Z; keycode++) {
-            if (Gdx.input.isKeyPressed(keycode)) {
-                return keycode; // Return the first pressed key found
-            }
-        }
-        return -1;
-    }
 
 }
