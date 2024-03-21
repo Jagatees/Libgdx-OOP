e34f59f0a2669fa529eaee5bad3377604f9aabdd
Jagatees <140966272+Jagatees@users.noreply.github.com>
2024-03-21 23:09:11 +0800

    Add : Change Image

--- OOPA1/core/src/com/mygdx/game/HealthyGame/Scene/GameScene.java
+++ OOPA1/core/src/com/mygdx/game/HealthyGame/Scene/GameScene.java
@@ -54,7 +54,7 @@ public class GameScene extends TemplateScene {
         entityManager = EntityManager.getInstance();
         EntityFactory entityFactory = new EntityFactory();
 
-        pacman = entityFactory.getEntityByInput("Player", "entity/pacman.png", 100, 100, 10, Entity.EntityState.NULL, false,  50, 50, Entity.EntityType.PLAYER, Entity.RenderType.SPRITE);
+        pacman = entityFactory.getEntityByInput("Player", "entity/bee.png", 100, 100, 10, Entity.EntityState.NULL, false,  50, 50, Entity.EntityType.PLAYER, Entity.RenderType.SPRITE);
 
         createWall("entity/wall.jpg", -20, 0, 15, 50, 0, true);
         createWall("entity/wall.jpg", 0, -20, 30, 50, 0, false);
