4039171678b35ae89323d3256a1ad69b2b526952
Jagatees <140966272+Jagatees@users.noreply.github.com>
2024-03-23 01:46:36 +0800

    Add : Collision Handler

---
+++ OOPA1/core/src/com/mygdx/game/LearningGame/LearningCollision/CollisionHandler.java
@@ -0,0 +1,51 @@
+package com.mygdx.game.LearningGame.LearningCollision;
+
+import com.mygdx.game.Engine.Entity.Entity;
+import com.mygdx.game.Engine.Entity.EntityManager;
+import com.mygdx.game.LearningGame.GameLogic.LearningGameLogic;
+import com.mygdx.game.Engine.Timer.Timer;
+
+import java.util.Objects;
+
+public class CollisionHandler {
+
+    private LearningGameLogic learningGameLogic = LearningGameLogic.getInstance();
+
+    public void handleCollision(Entity entity1, Entity entity2) {
+        Entity.EntityType entityType1 = EntityManager.getInstance().getType(entity1);
+        Entity.EntityType entityType2 = EntityManager.getInstance().getType(entity2);
+
+        String word = learningGameLogic.getCurrentWord();
+        char expectedLetter = learningGameLogic.getFirstLetterOfCurrentWordSafely(learningGameLogic.getScore());
+
+        if (learningGameLogic.getScore() != word.length()) {
+            if (Objects.equals(entityType2.getLetter(), String.valueOf(expectedLetter)) ||
+                    Objects.equals(entityType1.getLetter(), String.valueOf(expectedLetter))) {
+
+                if ((entityType1 == Entity.EntityType.PLAYER && entityType2.isLetter()) ||
+                        (entityType2 == Entity.EntityType.PLAYER && entityType1.isLetter())) {
+
+                    learningGameLogic.addScore(1);
+
+                    if (entityType1 == Entity.EntityType.PLAYER) {
+                        EntityManager.getInstance().removeEntity(entity2);
+                    } else {
+                        EntityManager.getInstance().removeEntity(entity1);
+                    }
+
+                    System.out.println("Current Difficulty: " + learningGameLogic.getCurrentDifficulty());
+                    System.out.println("Current Word: " + learningGameLogic.getCurrentWord());
+                    System.out.println("Current Score: " + learningGameLogic.getScore());
+
+                    if (learningGameLogic.getScore() != learningGameLogic.getCurrentWord().length()) {
+                        System.out.println("Next Letter: " + expectedLetter);
+                    }
+                }
+            } else {
+                // Wrong letter hit
+                System.out.println("Add time");
+                Timer.getInstance().addToTimer();
+            }
+        }
+    }
+}
